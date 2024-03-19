7623c52374a3428938ef3b97a14cd1ce3498ce81
Prasad <prasad@Prasads-MacBook-Air.local>
2024-03-19 09:08:20 +0530

    added class files in utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import connectDB from './db/index.js';
 import dotenv from 'dotenv';
-
 dotenv.config({
     path:'./env'
 });
 
-connectDB();
+app.
+
+connectDB()
+.then(()=>{
+    app.listen(prosess.env.PORT || 8000,()=>{
+      console.log(`Server Connected to PORT:${process.env.PORT}`);
+    })
+})
+.catch((err)=> console.error('MONGODB connection failed!',err));
+
